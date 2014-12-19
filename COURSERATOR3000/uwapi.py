97a42990d275af5f908a0931eca2bf29abbc875b
Uberi <azhang9@gmail.com>
2014-12-18 19:20:28 -0500

    Caching fix.

--- COURSERATOR3000/uwapi.py
+++ COURSERATOR3000/uwapi.py
@@ -3,8 +3,6 @@
 UW_API_KEY = "123afda14d0a233ecb585591a95e0339"
 UW_API_BASE = "http://api.uwaterloo.ca/v2/"
 
-import time
-
 import requests
 
 cached_responses = {} # the cached responses of each request
@@ -12,12 +10,12 @@ def uwapi(endpoint, params={}):
     try:
         if endpoint in cached_responses: # check if requested endpoint is cached
             print("retrieving", endpoint)
-            return cached_responses[endpoint]["data"]
+            return cached_responses[endpoint]
     except KeyError: pass # catch any KeyError instances upon invalid data
     print("querying", endpoint)
     params["key"] = UW_API_KEY
     r = requests.get(UW_API_BASE + endpoint + ".json", params=params)
     assert r.status_code == 200
     value = r.json()
-    cached_responses[endpoint] = value # update cache
+    cached_responses[endpoint] = value["data"] # update cache
     return value["data"]
