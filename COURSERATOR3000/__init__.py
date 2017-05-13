c2414016f738416f59f917113353ceecfd9bc800
Anthony Zhang <azhang9@gmail.com>
2017-05-12 21:16:23 -0700

    Update term ranges (also, file an issue with the API for functionality necessary to do this automatically https://github.com/uWaterloo/api-documentation/issues/101)

--- COURSERATOR3000/__init__.py
+++ COURSERATOR3000/__init__.py
@@ -19,7 +19,10 @@ class TermConverter(BaseConverter):
         "2015F": (1159, datetime(2015, 9, 8), datetime(2015, 12, 22)),
         "2016W": (1161, datetime(2016, 1, 4), datetime(2016, 4, 23)),
         "2016S": (1165, datetime(2016, 5, 2), datetime(2016, 8, 13)),
-        "2016F": (1159, datetime(2016, 9, 8), datetime(2016, 12, 22)),
+        "2016F": (1169, datetime(2016, 9, 6), datetime(2016, 12, 22)),
+        "2017W": (1171, datetime(2017, 1, 3), datetime(2017, 4, 25)),
+        "2017S": (1175, datetime(2017, 5, 1), datetime(2017, 8, 11)),
+        "2017F": (1179, datetime(2017, 9, 6), datetime(2016, 12, 22)),
     }
     
     def to_python(self, value):
